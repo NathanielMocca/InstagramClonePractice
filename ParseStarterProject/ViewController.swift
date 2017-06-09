f69aa585b81a21076ca2cd4af46e4b002030214a
Mocca Yang <tiffany12310@gmail.com>
2017-06-09 15:48:00 +0800

    change UInavigationbar's tint color

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -160,6 +160,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UINavigationBar.appearance().tintColor = UIColor.white
         
     }
     
